8fb9288007020014d0b8c1de5c7c4d2376ddda86
samuelmuittari <muittaris@gmail.com>
2023-04-12 15:34:50 +0300

    added bootstrap css

--- src/main/java/sm/workoutdiary/WorkoutdiaryApplication.java
+++ src/main/java/sm/workoutdiary/WorkoutdiaryApplication.java
@@ -31,7 +31,6 @@ public class WorkoutdiaryApplication {
 			User user2 = new User("admin", "$2a$10$0MMwY.IQqpsVc1jC8u7IJ.2rT8b0Cd3b3sfIBGV2zfgnPGtT4r0.C", "ADMIN");
 			userRepository.save(user1);
 			userRepository.save(user2);
-			 
 		};
 	}
 }
